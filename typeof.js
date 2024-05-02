14917ed672578bd3b0ffac347b56eb5879bb9ad1
Nicolli <148505390+hrckkl@users.noreply.github.com>
2024-05-02 19:51:49 -0300

    Create typeof.js

---
+++ typeof.js
@@ -0,0 +1,18 @@
+var numero = "16"
+var nome = "Nicolli"
+var numero2 = 16
+
+//console.log(typeof(numero))
+//console.log(typeof(nome))
+console.log(numero2)
+
+numero2 = parseFloat(numero2)
+console.log(numero2)
+
+
+//numero2 = toString(numero2)
+//console.log(typeof(numero2))
+
+ //numero = parseInt(numero)
+ //console.log(numero)
+ //console.log(typeof(numero))
