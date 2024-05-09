a3c01c4c0ebff09f68ef12622d2c7bb62ed8174a
Nicolli <148505390+hrckkl@users.noreply.github.com>
2024-05-09 15:22:34 -0300

    Create numero.js

---
+++ numero.js
@@ -0,0 +1,14 @@
+var numero1 = 5
+var numero2 = 8
+
+
+if(numero2 == numero1){
+  console.log("Ambos são iguai.")
+  
+}else if(numero2 > numero1){
+  console.log(numero2 + " é maior que " + numero1)
+    
+}else if(numero1 < numero2){
+  console.log(numero1 + " é maior que " + numero2)
+}
+
