8457500330dfe263b5a7758574db858e552b08fb
Nicolli <148505390+hrckkl@users.noreply.github.com>
2024-05-09 15:25:38 -0300

    Create positivonegativo.js

---
+++ positivonegativo.js
@@ -0,0 +1,10 @@
+var numero = 10
+
+if(numero >= 1){
+  console.log(numero + " é positivo.")
+} else if(numero <= -1){
+  console.log(numero + " é negativo.")
+} else {
+  console.log(numero + " é zero.")
+}
+
