a270ff620edf65872447d63b8e0ae6f2c76d81ad
Nicolli <148505390+hrckkl@users.noreply.github.com>
2024-05-09 23:43:43 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -1,13 +1,9 @@
 var num = prompt("você deseja a tabuada de qual numero?")
 num = parseInt(num)
 var resp
- for (var i = 1 ; i <= 5 ; i++){
+ for (var i = 1 ; i <= 10 ; i++){
    resp = num * i 
    console.log(resp)
- }*/
-// 5 
- for (var i = 1 ; i <= 10 ; i++){
-    for (var j = 1 ; j <= 10 ; j++){
-    console.log(`${i} x ${j} = ${i * j}`)
  }
- }
+
+ 
