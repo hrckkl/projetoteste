3435bc32e30042951952df16754a8b11d790e816
Nicolli <148505390+hrckkl@users.noreply.github.com>
2024-06-13 18:59:33 -0300

    Create laço,.py

---
+++ laço,.py
@@ -0,0 +1,16 @@
+media = 7
+while media < 10:
+    print(media)
+    media +=1
+
+/////////
+
+for numero in range(1,10,1): (inicial,final,encremento)
+for numero in range(1,10):
+   print(numero)
+
+//////////
+
+   palavra = "animal"
+   for letra in palavra:
+       print(letra)
